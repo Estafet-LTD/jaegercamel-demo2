0423c1c905c58ab9aa7b4ab3efaf7b10182ed71a
Ivo Costa <spacemanpt@gmail.com>
2017-09-28 17:18:38 +0100

    increased threading

--- src/main/java/com/estafet/batchprime/CamelBatchValidator.java
+++ src/main/java/com/estafet/batchprime/CamelBatchValidator.java
@@ -1,20 +1,31 @@
 package com.estafet.batchprime;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.ThreadPoolRejectedPolicy;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.spi.ThreadPoolProfile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CamelBatchValidator extends RouteBuilder {
+    @Autowired
+    CamelContext context;
 
     @Override
     public void configure() throws Exception {
+        ThreadPoolProfile profile = new ThreadPoolProfile("bigPool");
+        context.getExecutorServiceManager().registerThreadPoolProfile(profile);
+
+        profile.setMaxPoolSize(200);
+        profile.setRejectedPolicy(ThreadPoolRejectedPolicy.DiscardOldest);
         String validator_url = System.getenv("VALIDATOR_URL");
         from("netty4-http:http://0.0.0.0:8080/batchPrime")
                 .unmarshal().json(JsonLibrary.Jackson, PrimeBatch.class)
                 .split().simple("body.getPrimeList()")
-                .parallelProcessing()
+                .parallelProcessing().executorServiceRef("bigPool")
                     .convertBodyTo(String.class)
                     .setHeader(Exchange.HTTP_QUERY, simple("num=${body}"))
                     .to("http://"+validator_url+"/isPrime?bridgeEndpoint=true")
