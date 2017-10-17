66f4a3ce6d050bcaaf099d229401f397a15d325c
Ivo Costa <ivo.costa@estafet.com>
2017-10-17 12:17:48 +0100

    JMS added

--- src/main/java/com/estafet/batchprime/CamelBatchValidator.java
+++ src/main/java/com/estafet/batchprime/CamelBatchValidator.java
@@ -12,6 +12,8 @@ public class CamelBatchValidator extends RouteBuilder {
     public void configure() throws Exception {
 
         String validator_url = System.getenv("VALIDATOR_URL");
+        String rabbitmqUrl = System.getenv("RABBITMQ_URL");
+
         from("netty4-http:http://0.0.0.0:8080/batchPrime")
                 .unmarshal().json(JsonLibrary.Jackson, PrimeBatch.class)
                 .split().simple("body.getPrimeList()")
@@ -22,6 +24,7 @@ public class CamelBatchValidator extends RouteBuilder {
                     .convertBodyTo(String.class)
                     .to("log:foo")
                 .end()
+                .to("rabbitmq://" + rabbitmqUrl + "/amq.direct?autoDelete=false&routingKey=register")
                 .setBody()
                 .simple("All done here!");
 
